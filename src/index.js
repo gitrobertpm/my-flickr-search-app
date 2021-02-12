fd0b41f0685b9d4fad7afd0b8cf075e340a2ec6b
robertpm <grobertpm@gmail.com>
2021-02-12 03:48:12 -0800

    Setting env variables

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+require('dotenv').config();
 
 ReactDOM.render(
   <React.StrictMode>
